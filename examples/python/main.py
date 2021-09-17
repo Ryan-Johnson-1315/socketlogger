3c7986a1354ea3c7320615cd87cb2c89e7f6e9c9
Ryan Johnson <ryan.johnson@sdl.usu.edu>
2021-09-17 14:59:18 -0600

    update python examples to match the caller of the log/wrn etc function

--- examples/python/main.py
+++ examples/python/main.py
@@ -4,7 +4,8 @@ import socker
 
 if __name__ == '__main__':
     socker.start_logger("127.0.0.1", 0, 40000)
-
+    obj = {}
+    obj["name"] = "testing logging an object"
     for i in range(0, 1000):
         if i % 5 == 0:
             socker.log(f'UDP testing LOG from python {i}')
@@ -16,4 +17,5 @@ if __name__ == '__main__':
             socker.err(f'UDP testing DBG from python {i}')
         elif i % 1 == 0:
             socker.warn(f'UDP testing WARN from python {i}')
+            socker.warn(obj)
         time.sleep(.1)
--- examples/python/socker.py
+++ examples/python/socker.py
@@ -1,4 +1,4 @@
-from inspect import getframeinfo, currentframe
+import inspect
 import socket
 import json
 import queue
@@ -21,58 +21,57 @@ def start_logger(ip: str, client_port: int, server_port: int):
     global __s
     __s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     __s.bind((ip, client_port))
-    print(__s)
-    print((ip, client_port))
     global __initialized__
     __initialized__ = True
     threading.Thread(target=__send_msgs__, args=[(ip, server_port)]).start()
 
 
-def log(msg: str):
-    caller = getframeinfo(currentframe())
+def log(msg: any):
+    caller = inspect.getframeinfo(inspect.stack()[1][0])
     _, f = os.path.split(caller.filename)
-    __send__(MESSAGE_LVL_LOG, f'{f}:{caller.lineno}', msg)
+    __send__(MESSAGE_LVL_LOG, f"{f}:{caller.lineno}", msg)
 
 
-def warn(msg: str):
-    caller = getframeinfo(currentframe())
+def warn(msg: any):
+    caller = inspect.getframeinfo(inspect.stack()[1][0])
     _, f = os.path.split(caller.filename)
-    __send__(MESSAGE_LVL_WARN, f'{f}:{caller.lineno}', msg)
+    __send__(MESSAGE_LVL_WARN, f"{f}:{caller.lineno}", msg)
 
 
-def success(msg: str):
-    caller = getframeinfo(currentframe())
+def success(msg: any):
+    caller = inspect.getframeinfo(inspect.stack()[1][0])
     _, f = os.path.split(caller.filename)
-    __send__(MESSAGE_LVL_SUCCESS, f'{f}:{caller.lineno}', msg)
+    __send__(MESSAGE_LVL_SUCCESS, f"{f}:{caller.lineno}", msg)
 
 
-def dbg(msg: str):
-    caller = getframeinfo(currentframe())
+def dbg(msg: any):
+    caller = inspect.getframeinfo(inspect.stack()[1][0])
     _, f = os.path.split(caller.filename)
-    __send__(MESSAGE_LVL_DEBUG, f'{f}:{caller.lineno}', msg)
+    __send__(MESSAGE_LVL_DEBUG, f"{f}:{caller.lineno}", msg)
 
 
-def err(msg: str):
-    caller = getframeinfo(currentframe())
+def err(msg: any):
+    caller = inspect.getframeinfo(inspect.stack()[1][0])
     _, f = os.path.split(caller.filename)
-    __send__(MESSAGE_LVL_ERROR, f'{f}:{caller.lineno}', msg)
+    __send__(MESSAGE_LVL_ERROR, f"{f}:{caller.lineno}", msg)
 
 
-def __send__(lvl: int, caller: str, msg: str):
+def __send__(lvl: int, caller: str, msg: any):
     m = {}
     m["level"] = lvl
     m["caller"] = caller
+    if not isinstance(msg, str):
+        msg = f"{msg}"
     m["message"] = msg
 
     if __initialized__:
-        __q.put_nowait(bytes(json.dumps(m), encoding='utf-8'))
+        __q.put_nowait(bytes(json.dumps(m), encoding="utf-8"))
     else:
-        print(f'{m["caller"]} -- {m["message"]}')
+        print(
+            f'WARNING!!! Not initialized!!! msg: {m["caller"]} -- {m["message"]}')
 
 
 def __send_msgs__(addr):
-    print("sending to", __s)
     while True:
         bts = __q.get()
         __s.sendto(bts, addr)
-        # time.sleep(0.001)
